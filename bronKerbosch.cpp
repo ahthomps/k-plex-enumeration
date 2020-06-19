71a20d3ed666e0146e54099281499e20e551d1f9
ahthompson <almaotter@gmail.com>
2020-06-19 08:18:17 -0400

    efficient

--- bronKerbosch.cpp
+++ bronKerbosch.cpp
@@ -6,6 +6,7 @@
 BronKerbosch::BronKerbosch(std::vector<std::vector<int>> &_connected) {
   connected.swap(_connected);
   N = connected.size();
+  connected_bool.resize(N, false);
 }
 
 void BronKerbosch::solve() {
@@ -71,7 +72,7 @@ void BronKerbosch::solveFixP() {
   std::vector<int> X;
   for (int i = 0; i < (int) N; i++) P.push_back(i);
 
-  solveFixP_recursion(P, R, X);
+  solveFixP_recursion2(P, R, X);
 }
 
 void BronKerbosch::solveFixP_recursion(std::vector<int> &P, std::vector<int> &R, std::vector<int> &X) {
@@ -118,6 +119,56 @@ void BronKerbosch::solveFixP_recursion(std::vector<int> &P, std::vector<int> &R,
   }
 }
 
+void BronKerbosch::solveFixP_recursion2(std::vector<int> &P, std::vector<int> &R, std::vector<int> &X) {
+  if (P.size() == 0 && X.size() == 0) {
+    cliqueCounter++;
+    if (reportClique) {
+      for (int i = 0; i < R.size(); i++) {
+        std::cout << R[i] << ", ";
+      }
+      std::cout << std::endl;
+    }
+    return;
+  }
+  else if (P.size() == 0) return;
+
+  std::vector<bool> P_bool(N, false);
+  std::vector<bool> X_bool(N, false);
+  for (int p : P) P_bool[p] = true;
+  for (int x : X) X_bool[x] = true;
+
+  std::vector<int> candidates;
+  getFixP2(P, X, P_bool, candidates);
+
+  int p;
+  int sel;
+  for (int s : candidates) {
+
+    p = P[s];
+    P[s] = P[P.size() - 1];
+    P[P.size() - 1] = p;
+    sel = p;
+
+    std::vector<int> new_X;
+    std::vector<int> new_P;
+    for (int neighbor : connected[sel]) {
+      if (X_bool[neighbor]) new_X.push_back(neighbor);
+      if (P_bool[neighbor]) new_P.push_back(neighbor);
+    }
+
+    R.push_back(sel);
+
+    solveFixP_recursion2(new_P, R, new_X);
+
+    R.pop_back();
+    X.push_back(sel);
+    P.pop_back();
+
+    P_bool[sel] = false;
+    X_bool[sel] = true;
+  }
+}
+
 bool BronKerbosch::edge(int p, int q) {
   for (int r : connected[p]) {
     if (q == r) return true;
@@ -127,17 +178,24 @@ bool BronKerbosch::edge(int p, int q) {
 }
 
 void BronKerbosch::testFixP() {
-  std::vector<int> P{0,1};
-  std::vector<int> X{};
-  // for (int i = 0; i < (int) N; i++) P.push_back(i);
+  std::vector<int> P;
+  std::vector<int> X;
+  for (int i = 0; i < (int) N; i++) P.push_back(i);
+
+  std::vector<int> candidates;
+
+  std::vector<bool> P_bool(P.size(), false);
+  for (int p : P) P_bool[p] = true;
 
   int fixp, nod, s;
-  getFixP(P, X, fixp, nod, s);
-  std::cout << "fixp: " << fixp << std::endl;
-  std::cout << "s: " << s << std::endl << "nod: " << nod << std::endl;
+  getFixP2(P, X, P_bool, candidates);
+  std::cout << "candidates: ";
+  for (int c : candidates) std::cout << c << ", ";
+  std::cout << std::endl;
 }
 
 void BronKerbosch::getFixP(std::vector<int> &P, std::vector<int> &X, int &fixp, int &nod, int &s) {
+  // fix
   std::vector<std::vector<int>> PandX{P, X};
   int p;
   int count;
@@ -162,6 +220,36 @@ void BronKerbosch::getFixP(std::vector<int> &P, std::vector<int> &X, int &fixp, 
         if (index == 1) s = pos;
         else s = i;
       }
+    }
+  }
+}
+
+void BronKerbosch::getFixP2(std::vector<int> &P, std::vector<int> &X, std::vector<bool> &P_bool, std::vector<int> &candidates) {
+
+  std::vector<std::vector<int>*> L{&P, &X};
+  size_t counter, minnod;
+  std::vector<int> disconnections;
+  minnod = N + 1;
+
+  for (std::vector<int> *S : L) {
+    for (int node : *S) {
+      counter = 0;
+      disconnections.clear();
+      connected_bool[node] = true;
+      for (int neighbor : connected[node]) connected_bool[neighbor] = true;
+      if (P_bool[node]) disconnections.push_back(node);
+      for (int p : P) {
+        if (!connected_bool[p]) {
+          disconnections.push_back(p);
+          counter++;
+        }
+      }
+      if (counter < minnod) {
+        candidates = disconnections;
+        minnod = counter;
+      }
+      connected_bool[node] = false;
+      for (int neighbor : connected[node]) connected_bool[neighbor] = false;
     }
   }
 }
--- bronKerbosch.h
+++ bronKerbosch.h
@@ -19,15 +19,20 @@ class BronKerbosch {
     void solveFixP();
     void solveFixP_recursion(std::vector<int> &P, std::vector<int> &R, std::vector<int> &X);
 
+    void solveFixP_recursion2(std::vector<int> &P, std::vector<int> &R, std::vector<int> &X);
+
     void testFixP();
 
   private:
 
     std::vector<std::vector<int>> connected;
+    std::vector<bool> connected_bool;
     size_t N;
 
     bool edge(int p, int q);
     void getFixP(std::vector<int> &P, std::vector<int> &X, int &fixp, int &nod, int &s);
+    void getFixP2(std::vector<int> &P, std::vector<int> &X, std::vector<bool> &P_bool, std::vector<int> &candidates);
+
 };
 
 #endif
